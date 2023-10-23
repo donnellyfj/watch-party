6f75539a0ff4ef60efe215381a8b3b5bd3ee1ce4
donnellyfj <donnellyfj@gmail.com>
2023-10-23 18:45:25 -0400

    Update test for server compatability

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
+# Simple check to test connection on locally hosted server. If only one instance of this file
+# is connected to the server, there will be no response. If there are multiple instances, then
+# all other instances will receive a response. This is to mimic what happens when one user plays
+# or pauses the video that all users are watching. If the user who played/paused also received a
+# response from the server, their video would be thrown into a feedback loop where it is constantly
+# switching between being played/paused.
 
 import asyncio
 from websockets.sync.client import connect
+import json
+import time
 
 def hello():
     with connect("ws://localhost:8765") as websocket:
-        websocket.send("Hello world!")
+        websocket.send(json.dumps({'message': "Hello world!", 'id': time.localtime()}))
         message = websocket.recv()
         print(f"Received: {message}")
 
